5a264e4c06a24355897e3d319b31b5b968c32a76
Juha Yrjölä <juha.yrjola@iki.fi>
2017-04-28 17:07:53 +0300

    Hotfix: Cache HKM client results

--- hkm/hkm_client.py
+++ hkm/hkm_client.py
@@ -8,6 +8,9 @@ from StringIO import StringIO
 import requests
 from PIL import Image
 
+from django.core.cache import cache
+
+
 LOG = logging.getLogger(__name__)
 
 
@@ -20,6 +23,11 @@ class HKMClient(object):
         image. This checks the response headers to find out whether full res image exists or not and
         returns the full res image url if it does. Else returns None
         """
+        cache_key = 'hkm_%s' % preview_image_url
+        cached_value = cache.get(cache_key)
+        if cached_value:
+            return cached_value
+
         url_components = urlparse.urlparse(preview_image_url)
         qs_params = dict(urlparse.parse_qsl(url_components.query))
         qs_params['dataType'] = 'org'
@@ -32,6 +40,8 @@ class HKMClient(object):
         except requests.exceptions.RequestException:
             LOG.error('Failed to communicate with HKM image server',
                       exc_info=True)
+            # Cache negative result for an hour
+            cache.set(cache_key, None, 3600)
             return None
         else:
             try:
@@ -40,6 +50,8 @@ class HKMClient(object):
             except requests.exceptions.HTTPError:
                 LOG.error('Failed to communicate with HKM image server', exc_info=True,
                           extra={'data': {'status_code': r.status_code, 'response': repr(r.text)}})
+                # Cache negative result for an hour
+                cache.set(cache_key, None, 3600)
                 return None
 
         LOG.debug('Got result from HKM image server',
@@ -47,10 +59,13 @@ class HKMClient(object):
         try:
             content_type = r.headers['content-type']
             if 'image' in content_type:
+                # Cache positive result for a week
+                cache.set(cache_key, url, 7 * 24 * 3600)
                 return url
         except KeyError:
             pass
 
+        cache.set(cache_key, None, 3600)
         return None
 
     def download_image(self, full_res_url):
