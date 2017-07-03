f9f1919ec7d53978da2b26b8c047d4a98000096d
Sergei Koskello <sergei.koskello@andersinnovations.com>
2017-07-03 14:03:49 +0300

    Add translatable content model

--- hkm/admin.py
+++ hkm/admin.py
@@ -2,8 +2,10 @@
 
 from django.contrib import admin
 from django.contrib.auth.models import User
+from parler.admin import TranslatableAdmin
 
 from hkm import models
+
 
 admin.site.register(models.UserProfile)
 admin.site.register(models.Collection)
@@ -13,5 +15,6 @@ admin.site.register(models.PrintProduct)
 admin.site.register(models.ProductOrder)
 admin.site.register(models.Feedback)
 admin.site.register(models.TmpImage)
+admin.site.register(models.PageContent, TranslatableAdmin)
 
 
