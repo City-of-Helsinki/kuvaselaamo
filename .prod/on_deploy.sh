4b2b971abeabdfb3d5fec32ecabca54696b9dfb2
Kimmo Lempinen <kimmo.lempinen@capgemini.com>
2020-06-10 16:23:42 +0300

    HEL-16 | Add chmod +x to on_deploy.sh
    New try with the chmodding.

--- .prod/on_deploy.sh
+++ .prod/on_deploy.sh
@@ -3,7 +3,7 @@
 python /app/manage.py migrate --noinput
 
 # Generate the admin user using the password given in the environment variables.
-# If no password is set the admin user gets a generated password which will
+# If no password is set, the admin user gets a generated password which will
 # be written in stdout so that it can be accessed during the initial deployment.
 if [[ "$ADMIN_USER_PASSWORD" ]]; then
     python /app/manage.py add_admin_user -u admin -p $ADMIN_USER_PASSWORD -e admin@example.com
