821a6bbae60ba19af9d68fdfa64fdb190f6adc41
Kimmo Lempinen <kimmo.lempinen@capgemini.com>
2020-06-10 15:51:42 +0300

    HEL-16 | Fix typos in on_deploy.sh
    manage.py command was being called without python and also in the
    wrong directory.

--- .prod/on_deploy.sh
+++ .prod/on_deploy.sh
@@ -6,9 +6,9 @@ python /app/manage.py migrate --noinput
 # If no password is set, the admin user gets a generated password which will
 # be written in stdout so that it can be accessed during the initial deployment.
 if [[ "$ADMIN_USER_PASSWORD" ]]; then
-    ./manage.py add_admin_user -u admin -p $ADMIN_USER_PASSWORD -e admin@example.com
+    python /app/manage.py add_admin_user -u admin -p $ADMIN_USER_PASSWORD -e admin@example.com
 else
-    ./manage.py add_admin_user -u admin -e admin@example.com
+    python /app/manage.py add_admin_user -u admin -e admin@example.com
 fi
 
 python /app/manage.py create_initial_page_content
