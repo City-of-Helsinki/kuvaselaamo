769560aaa1edac6cd3f221c42047519aaad8f725
Kimmo Lempinen <kimmo.lempinen@capgemini.com>
2020-06-11 15:34:24 +0300

    HEL-16 | Comment out on_deploy.sh contents
    Trying to debug if it's really the on_deploy.sh contents which are
    causing issues in Kubernetes.

--- .prod/on_deploy.sh
+++ .prod/on_deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-python /app/manage.py migrate --noinput
+#python /app/manage.py migrate --noinput
 
 # Generate the admin user using the password given in the environment variables.
 # If no password is set, the admin user gets a generated password which will
 # be written in stdout so that it can be accessed during the initial deployment.
-if [[ "$ADMIN_USER_PASSWORD" ]]; then
-    python /app/manage.py add_admin_user -u admin -p $ADMIN_USER_PASSWORD -e admin@example.com
-else
-    python /app/manage.py add_admin_user -u admin -e admin@example.com
-fi
+#if [[ "$ADMIN_USER_PASSWORD" ]]; then
+#    python /app/manage.py add_admin_user -u admin -p $ADMIN_USER_PASSWORD -e admin@example.com
+#else
+#    python /app/manage.py add_admin_user -u admin -e admin@example.com
+#fi
 
-python /app/manage.py create_initial_page_content
+#python /app/manage.py create_initial_page_content
+
+echo "on_deploy.sh here ..."
 
