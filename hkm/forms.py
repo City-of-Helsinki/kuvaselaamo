d368ad7e774c22a3d3373c115ed5433ebea6a15a
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2020-07-20 14:11:11 +0300

    Change error string

--- hkm/forms.py
+++ hkm/forms.py
@@ -127,5 +127,5 @@ class ShowcaseForm(forms.ModelForm):
 
         if albums:
             if albums.count() > 3:
-                raise ValidationError('Albumien maksimimäärä on kolme')
+                raise ValidationError(u'Max amount of albums is three')
         return self.cleaned_data
