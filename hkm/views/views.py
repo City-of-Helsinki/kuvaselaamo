7edea2a007d3f813ff37362df94c6c96b9681ceb
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2020-07-21 11:52:36 +0300

    Reverse Showcase list.

--- hkm/views/views.py
+++ hkm/views/views.py
@@ -381,7 +381,7 @@ class HomeView(BaseView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['frontpage_image_collection'] = Collection.objects.filter(show_in_landing_page=True).order_by('created').first()
-        context['showcases'] = Showcase.objects.filter(show_on_home_page=True).order_by('created').reverse()
+        context['showcases'] = Showcase.objects.filter(show_on_home_page=True).order_by('-created')
         return context
 
 
