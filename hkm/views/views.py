1169f73dbb30b4024d468c9cd5c2ceeefd530a59
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2020-12-02 13:15:08 +0200

    Normal feedback form now shows message if submitting feedback is successfull.

--- hkm/views/views.py
+++ hkm/views/views.py
@@ -1322,7 +1322,8 @@ class RecordFeedbackView(View):
             record = request.POST.get("hkm_id", "")
             feedback = form.save(commit=False)
             feedback.record_id = record
-            feedback.sent_from = "".join([request.get_host(), '/search/record/?image_id=', record])
+            path = "/info/" if not record else '/search/record/?image_id=' + record
+            feedback.sent_from = "".join([request.get_host(), path])
             feedback.save()
             email.send_feedback_notification(feedback.id)
 
