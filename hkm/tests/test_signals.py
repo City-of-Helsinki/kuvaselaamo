2a18135d0ff5b77ea7b0bfe030aa392c14276bcf
Kimmo Lempinen <kimmo.lempinen@capgemini.com>
2021-04-01 09:10:23 +0300

    HEL-263 | Clear removal notification timestamp after login
    When we've sent a notification to a user telling that their account will be
    deleted after one month, the user can "cancel" the scheduled deletion by
    logging in to the service.
    
    I added a signal handler which sets a user's `removal_notification_sent`
    field to null after login. This will affect the scheduled removal so that the
    user won't be considered for deletion until 11 months later, if they don't
    login before that. At that point, they will get a new notification and their
    `removal_notification_sent` is set again.

---
+++ hkm/tests/test_signals.py
@@ -0,0 +1,22 @@
+import pytest
+from django.contrib.auth import signals
+from factories import UserFactory
+from django.test.client import RequestFactory
+from hkm.models.models import UserProfile
+from datetime import datetime, timedelta
+
+
+@pytest.mark.django_db
+def test_that_login_clears_removal_notification_timestamp():
+    user = UserFactory()
+
+    up = user.profile
+    up.removal_notification_sent = datetime.today() - timedelta(days=1)
+    up.save()
+    
+    assert UserProfile.objects.first().removal_notification_sent
+
+    request = RequestFactory().get("/login")
+    signals.user_logged_in.send(sender=user.__class__, request=request, user=user)
+
+    assert not UserProfile.objects.first().removal_notification_sent
