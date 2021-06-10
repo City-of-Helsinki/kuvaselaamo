c08d2da3a0b2851c03e108417e6d24682609a1f6
Kimmo Lempinen <kimmo.lempinen@capgemini.com>
2021-06-10 12:18:59 +0300

    HEL-299 | Add env flag for feedback congestion message
    Originally the "feedback is congested" message was hardcoded into the app
    as a quick way of putting the message on display. Now the feedback processing
    speed is back to normal and the message is no longer needed, but we decided
    to keep the message and control its visibility via an environment variable.
    
    I've now added the `ENABLE_FEEDBACK_CONGESTION_MSG` variable which is a
    boolean which controls whether the congestion message is displayed under the
    feedback form or not. In the future, should a similar congestion arise, we
    can just flip the env var and redeploy to get the message back out there.

--- kuvaselaamo/context_processors.py
+++ kuvaselaamo/context_processors.py
@@ -3,5 +3,6 @@ from django.conf import settings
 
 def global_settings(request):
     return {
-        'ENABLE_ANALYTICS': settings.ENABLE_ANALYTICS
+        'ENABLE_ANALYTICS': settings.ENABLE_ANALYTICS,
+        'ENABLE_FEEDBACK_CONGESTION_MSG': settings.ENABLE_FEEDBACK_CONGESTION_MSG
     }
