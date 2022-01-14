de76a59890e97138454ca4e07398ecb0e4047bd5
Juha Louhiranta <juha.louhiranta@anders.fi>
2022-01-14 11:59:14 +0200

    Remove UTF-8 formatting header
    In Python 3 it's the default.
    
    Refs HEL-311

--- hkm/admin.py
+++ hkm/admin.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from django import forms
 from django.conf.urls import url
 from django.contrib import admin
--- hkm/templatetags/hkm_tags.py
+++ hkm/templatetags/hkm_tags.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import logging
 from decimal import Decimal
 from random import randrange
