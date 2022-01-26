ee5f5a0f808d52e582ea0404121a6648a53c18e8
Juha Louhiranta <juha.louhiranta@anders.fi>
2022-01-26 23:46:41 +0200

    django.contrib.messages is required for Django admin
    Refs HEL-311

--- kuvaselaamo/settings.py
+++ kuvaselaamo/settings.py
@@ -176,6 +176,7 @@ INSTALLED_APPS = (
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
+    "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.admin.apps.SimpleAdminConfig",
     "anymail",
