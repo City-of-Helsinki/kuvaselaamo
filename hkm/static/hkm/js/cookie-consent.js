ad7b3835d7965d104b281c08e470566645a65e70
Ivan Melnik <melnik.job@gmail.com>
2023-11-01 19:20:05 +0200

    fix: cookie consent implementation

--- hkm/static/hkm/js/cookie-consent.js
+++ hkm/static/hkm/js/cookie-consent.js
@@ -1,18 +1,42 @@
 (function($) {
-    $(document).ready(function() {
+    var cookieName = "hki_consent"
+    var cookie = $.cookie(cookieName)
+    var _paq = window._paq = window._paq || [];
+
+    if(!cookie && _paq) {
+        _paq.push(['requireCookieConsent']);
+
         $("#cookie-consent").fadeIn();
         $( "#cookie-consent" ).animate({ "bottom": "0" }, "slow" );
 
-        $("#cookie-consent__readmore-btn").click(function(){
+        $("#cookie-consent__readmore-btn").on('click', function (e) {
             $("#cookie-consent__readmore-btn" ).hide();
             $("#cookie-consent__content" ).show();
         });
 
-        $("#cookie-consent").click(function(){
+        $("#cookie-consent").on('click', function (e) {
             $("#cookie-consent__readmore-btn" ).hide();
             $("#cookie-consent__content" ).show();
         });
+    }
+
+    $("#consent_accept").on('click', function (e) {
+        if(_paq) {
+            _paq.push(['setCookieConsentGiven']);
+            $.cookie(cookieName, 1, { expires : 393 });
+            $("#cookie-consent").fadeOut();
+        }
     });
+        
+    $("#consent_reject").on('click', function (e) {
+        if(_paq) {
+            _paq.push(['forgetCookieConsentGiven']);
+            $.cookie(cookieName, 0, { expires : 393 });
+            $("#cookie-consent").fadeOut();
+        }
+    });
+
+    // click outside to collapse cookie consent
     $(document).on('click', function (e) {
         if ($(e.target).closest("#cookie-consent").length === 0) {
             $("#cookie-consent__content").hide();
