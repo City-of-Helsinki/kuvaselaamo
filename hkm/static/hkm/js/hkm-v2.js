f80ec63142041abe596af8b8d7fbfebffaa284b6
Ivan Melnik <melnik.job@gmail.com>
2023-11-20 18:10:06 +0200

    fix: possible twitter fix

--- hkm/static/hkm/js/hkm-v2.js
+++ hkm/static/hkm/js/hkm-v2.js
@@ -332,8 +332,7 @@ palikka
     } else if($(this).hasClass('share-pin')) {
       sharerBaseUrl = 'http://pinterest.com/pin/create/button/';
       windowTitle = 'Pinterest -';
-      sharerParams = '?url=' + href;
-      // sharerParams = '?url=' + href + '&media=' + imageUrl + '&description=' + title;
+      sharerParams = '?url=' + href + '&media=' + imageUrl + '&description=' + title;
     }
     openWindow(window, sharerBaseUrl + sharerParams, windowTitle);
   });
